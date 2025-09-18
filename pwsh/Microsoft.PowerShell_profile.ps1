44eb4c8b2ce1b7d490a071d071f30ff11e0e1b6c
Preca Hoshino <001@labmem.com.cn>
2025-09-18 22:46:58 +0800

    Update Microsoft.PowerShell_profile.ps1

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,11 @@
-chcp 65001
-clear
-function Set-IPAddress([bool]$originalStatus) {
-    $userAgent = [Microsoft.PowerShell.Commands.PSUserAgent]::Chrome
-    echo "Get Ip`n"
-    $response = ConvertFrom-Json $(Invoke-WebRequest -Uri "https://api.ip.sb/geoip/" -UserAgent $userAgent | Select -ExpandProperty Content)
-    echo "Done`n"
-    $POSH_IP_REGION=$response.country_code
-    $POSH_IP=$response.ip
-}
+# function Set-IPAddress([bool]$originalStatus) {
+#     $userAgent = [Microsoft.PowerShell.Commands.PSUserAgent]::Chrome
+#     echo "Get Ip`n"
+#     $response = ConvertFrom-Json $(Invoke-WebRequest -Uri "https://api.ip.sb/geoip/" -UserAgent $userAgent | Select -ExpandProperty Content)
+#     echo "Done`n"
+#     $POSH_IP_REGION=$response.country_code
+#     $POSH_IP=$response.ip
+# }
 # New-Alias -Name 'Set-PoshContext' -Value 'Set-IPAddress' -Scope Global -Force
 Import-Module -Name Terminal-Icons
 Import-Module DirColors
@@ -20,10 +18,5 @@ Import-Module DirColors
 # Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 # Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 # Set-IPAddress
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/catppuccin_frappe.custom.json" | Invoke-Expression
+oh-my-posh init pwsh --config "https://raw.githubusercontent.com/preca-hoshino/dotfiles/refs/heads/main/.oh_my_posh/preca_custom.json" | Invoke-Expression
 # oh-my-posh init pwsh  | Invoke-Expression
-
-#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
-
-Import-Module -Name Microsoft.WinGet.CommandNotFound
-#f45873b3-b655-43a6-b217-97c00aa0db58
